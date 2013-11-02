ef1f4e8b019a1c2c4e5b08c60346b7ae97cd5052
srikanth <srikanth@ubuntu>
2013-11-01 21:29:01 -0400

    Average Word Count

---
+++ src/examples/AverageWordCount/AverageWordCount.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"mr"
+	"strings"
+	"strconv"
+)
+
+type WC struct{}
+
+// The input key, value will be filename, text
+// The output would be word, count
+func (wc WC) Mapper(key, value string, out chan mr.Pair) {
+	strr := strings.NewReader(value)
+	s := bufio.NewScanner(strr)
+	s.Split(bufio.ScanWords)
+	for s.Scan() {
+		word := s.Text()
+		ch:=word[0]
+		strng:=string(ch)
+		length:=len(word)
+		strlng:=strconv.Itoa(length)
+		out <- mr.Pair{strng, strlng}
+		//fmt.Println(word)
+	}
+	if err := s.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading file :", err)
+		os.Exit(-1)
+	}
+}
+
+// The reducer receives a word, <list of counts>
+// It adds up all the counts and outputs a word, combined_count
+func (wc WC) Reducer(key string, value []string, out chan mr.Pair) {
+	count := 0
+	for _, v := range value {
+		//fmt.Println("k: ", key, "v:", v)
+		c, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error converting \"", v, "\" to integer, err:", err)
+			os.Exit(-1)
+		}
+		count += c
+	}
+	avg:=len(value)
+	count=count/avg
+	out <- mr.Pair{key, strconv.Itoa(count)} 
+}
+
+func main() {
+	wc := WC{}
+	// Ouput all key-value pairs
+	out := mr.Run(wc, "/home/srikanth/New/")
+	for p := range out {
+		f := p.First
+		s := p.Second
+		fmt.Println(f, " ", s)
+	}
+	
+}
