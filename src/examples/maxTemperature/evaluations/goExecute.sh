2d70602b59d3c2f16949cae969dc3425d3e5eaea
Nitin Kosuri <nkosuri@lin116-20.cise.ufl.edu>
2013-11-09 18:56:06 -0500

    commiting test results

---
+++ src/examples/maxTemperature/evaluations/goExecute.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/one > test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/one >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/one >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/one >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/one >> test.txt
+
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/two >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/two >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/two >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/two >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/two >> test.txt
+
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/three >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/three >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/three >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/three >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/three >> test.txt
+
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/four >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/four >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/four >> test.txt
+
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/five >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/five >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/five >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/five >> test.txt
+go run src/examples/maxTemperature/maxTemperature.go ../goInput/five >> test.txt
